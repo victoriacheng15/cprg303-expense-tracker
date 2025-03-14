8deff85853e6600fb5e138e2146d50ecee2048e4
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-14 16:36:05 -0600

    implement pages: dashboard, charts, and settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function SettingsScreen() {
 	return (
 		<View style={styles.container}>
-			<Text>Home</Text>
+			<Text>SettingsScreen</Text>
 		</View>
 	);
 }
