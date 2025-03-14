6c06beac5c6f234bb2418feb60a13740db8cc6ef
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-14 16:35:06 -0600

    to text instead of view component

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFoundScreen() {
 		<>
 			<Stack.Screen options={{ title: "Oops!" }} />
 			<View style={styles.container}>
-				<View>This screen doesn't exist.</View>
+				<Text>This screen doesn't exist.</Text>
 				<Link href="/" style={styles.link}>
 					<Text>Go to home screen!</Text>
 				</Link>
