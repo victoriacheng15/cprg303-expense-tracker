7a205461456252606a8da501c13bb711dd3d1d80
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-15 20:36:25 -0600

    change mind to use bottom nav instead of side

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,78 +1,64 @@
-import { Alert } from "react-native";
-import { Drawer } from "expo-router/drawer";
-import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
-import { useSession } from "@/context/sessionContext";
+import Dashboard from "./dashboard";
+import ChartsScreen from "./charts";
+import SettingsScreen from "./settings";
+import Logout from "@/components/Logout";
+import { colors } from "@/constants/";
 
 export default function TabLayout() {
-	const { signOut } = useSession();
+	const Tab = createBottomTabNavigator();
 
-	async function handleSignOut() {
-		console.log("signing out");
-		Alert.alert(
-			"Sign Out",
-			"Are you sure you want to sign out?",
-			[
-				{
-					text: "Cancel",
-					style: "cancel",
-				},
-				{
-					text: "Sign Out",
-					style: "destructive",
-					onPress: async () => {
-						await signOut();
-					},
-				},
-			],
-			{ cancelable: true },
-		);
-	}
 	return (
-		<Drawer
-			drawerContent={(props) => (
-				<DrawerContentScrollView {...props}>
-					{/* Dashboard */}
-					<DrawerItem
-						label="Dashboard"
-						icon={({ color, size }) => (
-							<MaterialIcons name="dashboard" size={size} color={color} />
-						)}
-						onPress={() => props.navigation.navigate("dashboard")}
-					/>
-
-					{/* Charts */}
-					<DrawerItem
-						label="Analysis"
-						icon={({ color, size }) => (
-							<MaterialIcons name="bar-chart" size={size} color={color} />
-						)}
-						onPress={() => props.navigation.navigate("charts")}
-					/>
-
-					{/* Settings */}
-					<DrawerItem
-						label="Settings"
-						icon={({ color, size }) => (
-							<MaterialIcons name="settings" size={size} color={color} />
-						)}
-						onPress={() => props.navigation.navigate("settings")}
-					/>
-
-					{/* Sign Out */}
-					<DrawerItem
-						label="Sign Out"
-						icon={({ color, size }) => (
-							<MaterialIcons name="exit-to-app" size={size} color={color} />
-						)}
-						onPress={handleSignOut}
-					/>
-				</DrawerContentScrollView>
-			)}
+		<Tab.Navigator
+			screenOptions={{
+				headerTintColor: colors.neutral.lightGray,
+				headerStyle: { backgroundColor: colors.primary.darkBlue },
+				tabBarStyle: {
+					backgroundColor: colors.primary.darkBlue,
+				},
+				tabBarActiveTintColor: colors.neutral.lightGray,
+				tabBarInactiveTintColor: colors.neutral.mediumGray,
+			}}
 		>
-			<Drawer.Screen name="dashboard" options={{ title: "Dashboard" }} />
-			<Drawer.Screen name="charts" options={{ title: "Analysis" }} />
-			<Drawer.Screen name="settings" options={{ title: "Settings" }} />
-		</Drawer>
+			<Tab.Screen
+				name="Dashboard"
+				component={Dashboard}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<MaterialIcons name="dashboard" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Analysis"
+				component={ChartsScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<MaterialIcons name="bar-chart" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Settings"
+				component={SettingsScreen}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<MaterialIcons name="settings" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Logout"
+				component={EmptyScreen}
+				options={{
+					tabBarIcon: ({ size, color }) => <Logout size={size} color={color} />,
+				}}
+			/>
+		</Tab.Navigator>
 	);
+}
+
+export function EmptyScreen() {
+	return null;
 }
