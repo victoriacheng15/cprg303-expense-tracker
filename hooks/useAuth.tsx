080e2bc3464a6806e03a34170bf9c7d0877d82b7
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-23 20:38:43 -0600

    have signInWidthEmail and signOut in useAuth

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,22 +1,40 @@
+import { router } from "expo-router";
 import { supabase, redirectTo } from "@/lib/supabase";
-import { useSessionContext } from "@/context/sessionContext";
 
 export function useAuth() {
-	const { signOut } = useSessionContext();
+	async function signInWithEmail(email: string): Promise<void> {
+		console.log(`Redriect url: ${redirectTo}`);
+		try {
+			const { error } = await supabase.auth.signInWithOtp({
+				email,
+				options: {
+					emailRedirectTo: redirectTo,
+				},
+			});
 
-	async function signInWithEmail(email: string) {
-		console.log("Logging in...");
-		const { error } = await supabase.auth.signInWithOtp({
-			email,
-			options: {
-				emailRedirectTo: redirectTo,
-			},
-		});
-
-		if (error) throw error;
-		console.log("Logged in!");
+			if (error) {
+				throw new Error(error.message);
+			}
+		} catch (error) {
+			console.error("Error during sign-in:", error);
+		}
 	}
 
+	async function signOut(): Promise<void> {
+		try {
+			console.log("Signing out...");
+			const { error } = await supabase.auth.signOut();
+
+			if (error) {
+				throw new Error(error.message);
+			}
+
+			console.log("Signed out!");
+			router.replace("/");
+		} catch (error) {
+			console.error("Error during sign-out:", error);
+		}
+	}
 	return {
 		signInWithEmail,
 		signOut,
