f1e69ebf175cdb39308000f0a2255ccbb09c7fb1
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-24 15:43:35 -0600

    move getTransaction to the last in handleAdd

--- components/TransactionModal.tsx
+++ components/TransactionModal.tsx
@@ -33,9 +33,9 @@ export default function TransactionModal() {
 
 	function handleAdd() {
 		AddTransaction();
-		getTransactions();
 		setSelectedCategory(null);
 		resetTransaction();
+		getTransactions();
 	}
 
 	return (
