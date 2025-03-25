ce39b4018716dbc640e5a4e2424dcc9b6c171c93
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-25 09:41:24 -0600

    split type files into general, context and props

---
+++ types/contextType.d.ts
@@ -0,0 +1,24 @@
+interface SessionContextType {
+	session: Session | null;
+	signInWithEmail: (email: string) => Promise<void>;
+	signOut: () => Promise<void>;
+}
+
+interface TransactionsContextType {
+	transactions: TransactionItem[];
+	transactionItem: TransactionItem;
+	modalVisible: boolean;
+	datePickerConfig: DatePickerConfig;
+	setModalVisible: (visible: boolean) => void;
+	showDatepicker: () => void;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	onDateChange: (_: any, selectedDate?: Date) => void;
+	updateTransaction: (
+		field: keyof TransactionItem,
+		value: string | number,
+	) => void;
+	resetTransaction: () => void;
+	AddTransaction: () => Promise<void>;
+	deleteTransaction: (id: string) => Promise<void>;
+	getTransactions: () => void;
+}
