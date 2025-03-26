f86b90e8ee24636a8bbf4c7c47f6072afa7f46e9
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-26 17:42:16 -0600

    implement pie charts for categories based on year/month and year

--- app/(tabs)/charts.tsx
+++ app/(tabs)/charts.tsx
@@ -1,19 +1,46 @@
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import { PieChart } from "react-native-chart-kit";
 import { globalStyle } from "@/constants/";
 import { useVisualization } from "@/hooks/useVisualization";
+import { useGenerateDistinctColor } from "@/hooks/useGenerateDistinctColor";
 import VisualizationFilter from "@/components/VisualizationFilter";
 
 export default function ChartsScreen() {
 	const {
-		filterTypes,
-		availableYears,
+		filterTypes: { year, month, category },
+		filteredYears,
 		filteredMonths,
-		availableCategories,
+		filteredCategories,
 		handleFilterSelect,
-		clearFilters,
+		incomeCategories,
+		getMonthlyTransactions,
+		getYearlyTransactions,
+		getMonthLabel,
 	} = useVisualization();
-	const { year, month, category } = filterTypes;
-	const noFiltersSelected = !year && !month && !category;
+	const { distinctColors } = useGenerateDistinctColor();
+	const { width } = Dimensions.get("window");
+
+	const categoryMonthlyData = getMonthlyTransactions.reduce<
+		Record<string, number>
+	>((acc, t) => {
+		const cat = t.category_name;
+		if (!incomeCategories.includes(cat)) {
+			acc[cat] = (acc[cat] || 0) + t.amount;
+		}
+
+		return acc;
+	}, {});
+
+	const categoryYearlyData = getYearlyTransactions.reduce<
+		Record<string, number>
+	>((acc, t) => {
+		const cat = t.category_name;
+		if (!incomeCategories.includes(cat)) {
+			acc[cat] = (acc[cat] || 0) + t.amount;
+		}
+
+		return acc;
+	}, {});
 
 	return (
 		<View style={globalStyle.container}>
@@ -22,22 +49,68 @@ export default function ChartsScreen() {
 					year={year}
 					month={month}
 					category={category}
-					availableYears={availableYears}
+					filteredYears={filteredYears}
 					filteredMonths={filteredMonths}
-					availableCategories={availableCategories}
+					filteredCategories={filteredCategories}
 					handleFilterSelect={handleFilterSelect}
 				/>
-				{/* Clear Filters Button */}
-				<Pressable
-					style={[
-						styles.clearButton,
-						noFiltersSelected && styles.disabledClearButton,
-					]}
-					onPress={clearFilters}
-					disabled={noFiltersSelected}
-				>
-					<Text style={styles.clearButtonText}>Clear Filters</Text>
-				</Pressable>
+				<ScrollView>
+					<View style={styles.visualization}>
+						{/* Render your chart component here */}
+						<View style={styles.section}>
+							<Text style={styles.sectionTitle}>
+								Spending Data for {getMonthLabel(month)}, {year}
+							</Text>
+							{Object.entries(categoryMonthlyData).length > 0 ? (
+								<PieChart
+									data={Object.entries(categoryMonthlyData)
+										.sort((a, b) => b[1] - a[1])
+										.map(([category, amount], index) => ({
+											name: category,
+											amount,
+											color: distinctColors[index],
+											legendFontColor: "#7F7F7F",
+										}))}
+									width={width * 0.9}
+									height={200}
+									accessor="amount"
+									backgroundColor="transparent"
+									paddingLeft={"5"}
+									chartConfig={{
+										color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+									}}
+									absolute
+								/>
+							) : (
+								<Text>No expenses this month</Text>
+							)}
+						</View>
+						<View style={styles.section}>
+							<Text style={styles.sectionTitle}>
+								Category Spending Data for {year}
+							</Text>
+							<PieChart
+								data={Object.entries(categoryYearlyData)
+									.sort((a, b) => b[1] - a[1])
+									.map(([category, amount], index) => ({
+										name: category,
+										amount,
+										color: distinctColors[index],
+										legendFontColor: "#7F7F7F",
+									}))}
+								width={width * 0.9}
+								height={200}
+								accessor="amount"
+								backgroundColor="transparent"
+								paddingLeft={"15"}
+								chartConfig={{
+									color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+								}}
+								absolute
+							/>
+						</View>
+					</View>
+				</ScrollView>
 			</View>
 		</View>
 	);
@@ -48,23 +121,20 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#fff",
 		width: "100%",
-		padding: 20,
-		borderRadius: 10,
+		padding: 16,
+		borderRadius: 5,
 	},
-	clearButton: {
+	visualization: {
 		marginTop: 16,
-		padding: 12,
-		backgroundColor: "#ff3b30",
-		borderRadius: 8,
 		alignItems: "center",
+		gap: 16,
 	},
-	clearButtonText: {
-		color: "white",
-		fontWeight: "bold",
+	sectionTitle: {
 		fontSize: 16,
+		fontWeight: "bold",
 	},
-	disabledClearButton: {
-		backgroundColor: "#cccccc",
-		opacity: 0.8,
+	section: {
+		justifyContent: "center",
+		alignItems: "center",
 	},
 });
