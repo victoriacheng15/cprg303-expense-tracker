4b5ae0da046baa10d9fb356988d9e53fd53d72cf
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-30 19:41:32 -0600

    refator to PieVisialization and implement reuseable setupChartdata function

--- app/(tabs)/charts.tsx
+++ app/(tabs)/charts.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View, Text, ScrollView } from "react-native";
-import { PieChart } from "react-native-chart-kit";
-import { globalStyle, screenWidth } from "@/constants/";
+import { globalStyle } from "@/constants/";
 import { useVisualization } from "@/hooks/useVisualization";
 import { useGenerateDistinctColor } from "@/hooks/useGenerateDistinctColor";
 import VisualizationFilter from "@/components/VisualizationFilter";
+import PieVisualization from "@/components/PieVisualization";
 
 export default function ChartsScreen() {
 	const {
@@ -17,6 +17,24 @@ export default function ChartsScreen() {
 		yearlyData,
 	} = useVisualization();
 	const { distinctColors } = useGenerateDistinctColor();
+
+	function setupChartData(data: Record<string, number>) {
+		return Object.entries(data)
+			.sort((a, b) => b[1] - a[1])
+			.map(([category, amount], index) => ({
+				name: category,
+				amount,
+				color: distinctColors[index],
+				legendFontColor: "#7F7F7F",
+				legendFontSize: 14,
+			}));
+	}
+
+	const monthIncomeDistribution = setupChartData(monthlyData.incomes);
+	const monthSpendingDistribution = setupChartData(monthlyData.spendings);
+
+	const yearIncomeDistribution = setupChartData(yearlyData.incomes);
+	const yearSpendingDistribution = setupChartData(yearlyData.spendings);
 
 	return (
 		<View style={globalStyle.container}>
@@ -32,99 +50,32 @@ export default function ChartsScreen() {
 				/>
 				<ScrollView style={{ flex: 1 }}>
 					<View style={styles.visualization}>
-						{/* Render your chart component here */}
+						{/* Monthly Visualizations */}
 						<View style={styles.section}>
 							<Text style={styles.sectionTitle}>
 								Incomes and Spendings for {getMonthLabel(month)}, {year}
 							</Text>
+
 							<Text style={styles.sectionSubTitle}>Income Distribution</Text>
-							<PieChart
-								data={Object.entries(monthlyData.incomes)
-									.sort((a, b) => b[1] - a[1])
-									.map(([category, amount], index) => ({
-										name: category,
-										amount,
-										color: distinctColors[index],
-										legendFontColor: "#7F7F7F",
-										legendFontSize: 14,
-									}))}
-								width={screenWidth * 0.9}
-								height={200}
-								accessor="amount"
-								backgroundColor="transparent"
-								paddingLeft={"15"}
-								chartConfig={{
-									color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-								}}
-							/>
+							<PieVisualization data={monthIncomeDistribution} />
+
 							<Text style={styles.sectionSubTitle}>Spending Distribution</Text>
-							<PieChart
-								data={Object.entries(monthlyData.spendings)
-									.sort((a, b) => b[1] - a[1])
-									.map(([category, amount], index) => ({
-										name: category,
-										amount,
-										color: distinctColors[index],
-										legendFontColor: "#7F7F7F",
-										legendFontSize: 14,
-									}))}
-								width={screenWidth * 0.9}
-								height={200}
-								accessor="amount"
-								backgroundColor="transparent"
-								paddingLeft={"15"}
-								chartConfig={{
-									color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-								}}
-							/>
+							<PieVisualization data={monthSpendingDistribution} />
 						</View>
+
+						{/* Yearly Visualizations */}
 						<View style={styles.section}>
 							<Text style={styles.sectionTitle}>
 								Incomes and Spendings for {year}
 							</Text>
+
 							<Text style={styles.sectionSubTitle}>Income Distribution</Text>
-							<PieChart
-								data={Object.entries(yearlyData.incomes)
-									.sort((a, b) => b[1] - a[1])
-									.map(([category, amount], index) => ({
-										name: category,
-										amount,
-										color: distinctColors[index],
-										legendFontColor: "#7F7F7F",
-										legendFontSize: 14,
-									}))}
-								width={screenWidth * 0.9}
-								height={200}
-								accessor="amount"
-								backgroundColor="transparent"
-								paddingLeft={"15"}
-								chartConfig={{
-									color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-								}}
-							/>
+							<PieVisualization data={yearIncomeDistribution} />
+
 							<Text style={styles.sectionSubTitle}>Spending Distribution</Text>
-							<PieChart
-								data={Object.entries(yearlyData.spendings)
-									.sort((a, b) => b[1] - a[1])
-									.map(([category, amount], index) => ({
-										name: category,
-										amount,
-										color: distinctColors[index],
-										legendFontColor: "#7F7F7F",
-										legendFontSize: 14,
-									}))}
-								width={screenWidth * 0.9}
-								height={200}
-								accessor="amount"
-								backgroundColor="transparent"
-								paddingLeft={"15"}
-								chartConfig={{
-									color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-								}}
-							/>
+							<PieVisualization data={yearSpendingDistribution} />
 						</View>
 					</View>
-					<View style={styles.section}>{/*  */}</View>
 				</ScrollView>
 			</View>
 		</View>
